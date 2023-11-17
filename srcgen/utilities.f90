0e5f5f5543cff6114d7364804748452ab36dabe7
Milan Skocic <milan.skocic@icloud.com>
2023-11-17 07:15:41 +0100

    ENH: Update parser.

--- srcgen/utilities.f90
+++ srcgen/utilities.f90
@@ -13,7 +13,16 @@ module utils
     
     integer(int32), parameter :: LINE_LENGTH = 256 !! Length of line.
     integer(int32), parameter :: cix_element(2) = [1, 10]
-    integer(int32), parameter :: cix_compute(2) = [124, 133]
+    integer(int32), parameter :: cix_symbol(2) = [11, 17]
+    integer(int32), parameter :: cix_z(2) = [18, 22]
+    integer(int32), parameter :: cix_saw_min(2) = [23, 42]
+    integer(int32), parameter :: cix_saw_max(2) = [43, 62]
+    integer(int32), parameter :: cix_saw(2) = [63, 82]
+    integer(int32), parameter :: cix_saw_u(2) = [83, 92]
+    integer(int32), parameter :: cix_footnote(2) = [93, 102]
+    integer(int32), parameter :: cix_asaw(2) = [103, 112]
+    integer(int32), parameter :: cix_asaw_u(2) = [113, 122]
+    integer(int32), parameter :: cix_compute(2) = [123, 132]
 
 contains
 
@@ -92,6 +101,17 @@ subroutine write_saw_data(fciaaw, ffortran, props)
     character(len=LINE_LENGTH) :: line
     character(len=10) :: element
     logical :: compute
+    real(real64) :: saw_max_db
+    character(len=20) :: saw_max
+    real(real64) :: saw_min_db
+    character(len=20) :: saw_min
+    real(real64) :: saw_value_db
+    character(len=20) :: saw_value
+    real(real64) :: saw_u_db
+    character(len=20) :: saw_u
+    character(len=20) :: asaw
+    character(len=20) :: asaw_u
+    
     integer(int32) :: i
     
     rewind(unit=fciaaw)
@@ -107,7 +127,28 @@ subroutine write_saw_data(fciaaw, ffortran, props)
         read(fciaaw, "(A)") line
         if(len(trim(line))>0)then
             read(line(cix_compute(1): cix_compute(2)), "(L4)") compute
+            read(line(cix_saw_min(1): cix_saw_min(2)), "(A)") saw_min
+            read(line(cix_saw_max(1): cix_saw_max(2)), "(A)") saw_max
+            read(line(cix_saw(1): cix_saw(2)), "(A)") saw_value
+            read(line(cix_saw_u(1): cix_saw_u(2)), "(A)") saw_u
+            read(line(cix_asaw(1): cix_asaw(2)), "(A)") asaw
+            read(line(cix_asaw_u(1): cix_asaw_u(2)), "(A)") asaw_u
+            
             print *, compute
+
+            if(compute)then
+                read(saw_max, "(D20.10)") saw_max_db
+                read(saw_min, "(D20.10)") saw_min_db
+                saw_value_db = (saw_max_db + saw_min_db)/2.0d0
+                saw_u_db = (saw_max_db - saw_min_db)/(2.0d0*sqrt(3.0d0))
+                write(saw_value, "(ES19.12E2)") saw_value_db
+                write(saw_u, "(ES19.12E2)") saw_u_db
+            else
+                saw_value = ""
+                saw_u = ""
+            end if
+            print *, saw_min, saw_max, saw_value, saw_u, asaw, asaw_u
+        else
         endif
 
     end do
