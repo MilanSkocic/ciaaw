23ef644e3b5144a0fdfc9eda4a59bebbd24f9326
Milan Skocic <milan.skocic@icloud.com>
2023-12-04 13:08:39 +0100

    TST: Add test for pywrapper.

---
+++ pywrapper/pyciaaw/tests/test_saw.py
@@ -0,0 +1,25 @@
+r"""Tests"""
+import unittest
+from .. import saw
+import numpy as np
+
+
+class TestkH(unittest.TestCase):
+    r"""Test ciaaw library."""
+    def test_asaw(self):
+        N = 2
+        i = 0
+        expected = [0.0] * N
+        elements = [{}] * N
+
+        expected[0] = 1.0080
+        expected[1] = 12.011
+
+        elements[0] = saw.H
+        elements[1] = saw.C
+
+        for i in range(N):
+            with self.subTest():
+                diff = elements[i]["asaw"] - expected[i]
+                self.assertEqual(diff, 0.0, msg=f"   {elements[i]["symbol"]:s}    {elements[i]["asaw"]:+23.16f}/{expected[i]}/{diff:+23.16f}")
+    
