2b135cc29056983b20ebe992ce99936fa817a50a
Milan Skocic <milan.skocic@icloud.com>
2023-12-15 22:59:48 +0100

    MAINT: Clean up setp.py

--- pywrapper/setup.py
+++ pywrapper/setup.py
@@ -26,6 +26,4 @@ if __name__ == "__main__":
                         library_dirs=library_dirs,
                         runtime_library_dirs=runtime_library_dirs,
                         extra_objects=extra_objects)
-    
     setup(ext_modules=[mod_ext])
-    
