630b1f8619429f0a444a4ecec4a0e9c1c43ee0e1
Milan Skocic <milan.skocic@icloud.com>
2024-02-22 07:28:06 +0100

    BLD: Remove unecessary libs and headers in pywrapper.

--- pywrapper/src/pyciaaw/ciaaw_version.h
+++ pywrapper/src/pyciaaw/ciaaw_version.h
@@ -1,10 +1,15 @@
 /**
 * @file
-* @brief Version
+* @brief C API Version
 */
 
 #ifndef ciaaw_VERSION_H
 #define ciaaw_VERSION_H
+
+/**
+ * @brief Get the version.
+ * @return char pointer of the version string.
+ */
 extern char* ciaaw_get_version(void);
 #endif
 
