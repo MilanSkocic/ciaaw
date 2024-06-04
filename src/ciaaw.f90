812c270ccaa62b315a799ecca747242f48e6668e
Milan Skocic <milan.skocic@icloud.com>
2024-06-04 06:15:51 +0200

    DOC: Fix docstrings for modules.

--- src/ciaaw.f90
+++ src/ciaaw.f90
@@ -1,8 +1,5 @@
-!> @file
-!! @brief Main module for CIAAW.
-
-!> @brief Main module for CIAAW.
 module ciaaw
+    !! Main module for the Ciaaw library.
     use ciaaw__version
     use ciaaw__saw
 end module
