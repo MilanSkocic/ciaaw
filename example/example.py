26a522aef14f7a03ade228401fb234b4d2edbf98
Milan Skocic <milan.skocic@icloud.com>
2024-10-10 04:49:23 +0200

    Update python example with ICE.

--- example/example.py
+++ example/example.py
@@ -6,8 +6,16 @@ import pyciaaw
 print("########## CIAAW VERSION ##########")
 print("version ", pyciaaw.__version__)
 
-print("########## CIAAW SAW - by atomic number ##########")
+print("########## CIAAW SAW  ##########")
 print("Abridged Standard Atomic Weight: ", pyciaaw.get_saw("H"))
 print("Uncertainty for abridged Standard Atomic Weight: ", pyciaaw.get_saw("H", uncertainty=True))
 print("Standard Atomic Weight: ", pyciaaw.get_saw("H", abridged=False, uncertainty=False))
 print("Uncertainty for Standard Atomic Weight: ", pyciaaw.get_saw("H", abridged=False, uncertainty=True))
+
+
+print("########## CIAAW ICE  ##########")
+print("Abridged Standard Atomic Weight: ", pyciaaw.get_saw("H"))
+print('ICE H 1   = ', pyciaaw.get_ice("H", A=1))
+print('U ICE H 1 = ', pyciaaw.get_ice("H", A=1, uncertainty=True))
+print('ICE H 2   = ', pyciaaw.get_ice("H", A=2))
+print('U ICE H 2 = ', pyciaaw.get_ice("H", A=2, uncertainty=True))
