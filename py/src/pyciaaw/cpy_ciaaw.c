22e9f8a06f723685ca9ea59b13302fe903d66e16
Milan Skocic <milan.skocic@icloud.com>
2024-10-17 21:33:10 +0200

    Update python wrapper.

--- py/src/pyciaaw/cpy_ciaaw.c
+++ py/src/pyciaaw/cpy_ciaaw.c
@@ -7,12 +7,18 @@
 PyDoc_STRVAR(module_docstring, "C extension for ciaaw.");
 
 PyDoc_STRVAR(get_saw_doc, 
-"get_saw(s: str, abridged: bool, uncertainty: bool) --> float \n\n"
+"get_saw(s: str, abridged: bool, uncertainty: bool) -> float \n\n"
 "Get the abridged standard atomic weight. Returns NaN if not found.");
 
 PyDoc_STRVAR(get_ice_doc, 
-"get_saw(s: str, A: int, uncertainty: bool) --> float \n\n"
-"Get the abridged standard atomic weight. Returns -NaN if not found.");
+"get_ice(s: str, A: int, uncertainty: bool) -> float \n\n"
+"Get isotopic composition of the element for the mass number A. Returns -NaN if not found.");
+ 
+
+PyDoc_STRVAR(get_nice_doc,
+"get_nice(s: str) -> int \n\n"
+"Get the number of isotopes in ICE. Returns -1 if not found.");
+
 
 
 
@@ -54,9 +60,24 @@ static PyObject *get_ice(PyObject *self, PyObject *args, PyObject *kwargs){
 
 
 
+static PyObject *get_nice(PyObject *self, PyObject *args){
+    char *s;
+    int res;
+
+    if (!PyArg_ParseTuple(args, "s", &s)){
+        return NULL;
+    }
+    res = ciaaw_get_nice(s, strlen(s));
+
+    return Py_BuildValue("i", res);
+}
+
+
+
 static PyMethodDef myMethods[] = {  
-    {"get_saw", (PyCFunction) get_saw, METH_VARARGS | METH_KEYWORDS, get_saw_doc},
-    {"get_ice", (PyCFunction) get_ice, METH_VARARGS | METH_KEYWORDS, get_ice_doc},
+    {"get_saw",  (PyCFunction) get_saw,  METH_VARARGS | METH_KEYWORDS, get_saw_doc},
+    {"get_ice",  (PyCFunction) get_ice,  METH_VARARGS | METH_KEYWORDS, get_ice_doc},
+    {"get_nice", (PyCFunction) get_nice, METH_VARARGS                , get_nice_doc},
     { NULL, NULL, 0, NULL }};
 
 static struct PyModuleDef ciaaw = {PyModuleDef_HEAD_INIT, "ciaaw", module_docstring, -1, myMethods};
