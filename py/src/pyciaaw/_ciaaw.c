5db33b285eb5c9fd59d9c37f81be123c6d4924e1
Milan Skocic <milan.skocic@icloud.com>
2024-10-29 07:05:53 +0100

    Fix error in C extension.

--- py/src/pyciaaw/_ciaaw.c
+++ py/src/pyciaaw/_ciaaw.c
@@ -1,7 +1,5 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
-#include <stdbool.h>
-#include <string.h>
 #include "ciaaw.h"
 
 
@@ -25,16 +23,15 @@ PyDoc_STRVAR(get_nice_doc,
 static PyObject *get_saw(PyObject *self, PyObject *args){
     
     char *s;
-    bool abridged;
-    bool uncertainty;
-    int size;
+    int abridged;
+    int uncertainty;
+    Py_ssize_t size;
     double res;
 
-    if (!PyArg_ParseTuple(args, "spp", &s, &abridged, &uncertainty)){
+    if (!PyArg_ParseTuple(args, "s#pp", &s, &size, &abridged, &uncertainty)){
         return NULL;
     }
-    size = strlen(s);
-    res = ciaaw_get_saw(s, size, abridged, uncertainty);
+    res = ciaaw_get_saw(s, size, (bool) abridged, (bool) uncertainty);
     
     return Py_BuildValue("d", res);
 }
@@ -45,15 +42,14 @@ static PyObject *get_ice(PyObject *self, PyObject *args){
     
     char *s;
     int A;
-    int size;
-    bool uncertainty;
+    Py_ssize_t size;
+    int uncertainty;
     double res;
 
-    if (!PyArg_ParseTuple(args, "sip", &s, &A, &uncertainty)){
+    if (!PyArg_ParseTuple(args, "s#ip", &s, &size, &A, &uncertainty)){
         return NULL;
     }
-    size = strlen(s);
-    res = ciaaw_get_ice(s, size, A, uncertainty);
+    res = ciaaw_get_ice(s, size, A, (bool) uncertainty);
     
     return Py_BuildValue("d", res);
 }
@@ -63,13 +59,12 @@ static PyObject *get_ice(PyObject *self, PyObject *args){
 static PyObject *get_nice(PyObject *self, PyObject *args){
     
     char *s;
-    int size;
+    Py_ssize_t size;
     int res;
 
-    if (!PyArg_ParseTuple(args, "s", &s)){
+    if (!PyArg_ParseTuple(args, "s#", &s, &size)){
         return NULL;
     }
-    size = strlen(s);
     res = ciaaw_get_nice(s, size);
 
     return Py_BuildValue("i", res);
@@ -88,5 +83,13 @@ static struct PyModuleDef _ciaaw = {PyModuleDef_HEAD_INIT, "_ciaaw", module_docs
 
 
 PyMODINIT_FUNC PyInit__ciaaw(void){
-     return PyModule_Create(&_ciaaw);
+    PyObject *m;
+    PyObject *d;
+    PyObject *v;
+    m = PyModule_Create(&_ciaaw);
+    d = PyModule_GetDict(m);
+    v = PyUnicode_FromFormat("%s", ciaaw_get_version());
+    PyDict_SetItemString(d, "__version__", v);
+    Py_INCREF(v);
+    return m;
 }
