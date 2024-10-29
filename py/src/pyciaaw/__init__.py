6b753afe02e40a434a4936e713dbaf170dd797c9
Milan Skocic <milan.skocic@icloud.com>
2024-10-29 07:07:27 +0100

    Update docstrings for pure python wrappers.

--- py/src/pyciaaw/__init__.py
+++ py/src/pyciaaw/__init__.py
@@ -3,5 +3,5 @@
 Standard Atomic Weights: the lattest values for are from 2021.
 
 """
-from .version import __version__
+from .ciaaw import __version__
 from .ciaaw import *
--- py/src/pyciaaw/ciaaw.py
+++ py/src/pyciaaw/ciaaw.py
@@ -1,6 +1,6 @@
 """CIAAW"""
+from ._ciaaw import __version__
 from . import _ciaaw
-
 
 def get_saw(s: str, abridged: bool=True, uncertainty: bool=False)->float:
     r"""
@@ -21,13 +21,44 @@ def get_saw(s: str, abridged: bool=True, uncertainty: bool=False)->float:
     saw: float
         Returns NaN if the provided symbol is incorrect or -1 if the element does not have a standard atomic weight.
     """
+    return _ciaaw.get_saw(str(s), bool(abridged), bool(uncertainty))
 
 
 def get_nice(s):
+    r"""
+    Get the number of isotopes in ICE.
 
-    return _ciaaw.get_nice(s)
+    Parameters
+    ----------
+    s: str
+        Element symbol.
+    
+    Returns
+    -------
+    nice: int
+        Number of isotopes in ICE. 
+        Returns -1 if the provided symbol is incorrect.
+    """
+    return _ciaaw.get_nice(str(s))
 
 
 def get_ice(s:str, A:int, uncertainty: bool=False):
-    
-    return _ciaaw.get_ice(s, A, uncertainty)
+    r"""
+    Get the isotopic composition of the element for the mass number A. 
+
+    Parameters
+    ----------
+    s: str
+        Element symbol.
+    A: int
+        Mass number.
+    uncertainty: bool, optional
+        Flag to get the uncertainty instead of the value. Default to False.
+
+    Returns
+    -------
+    ice: float
+        Isotopic composition.
+        Returns NaN if provided symbol or A are incorrect or -1 if the element does not have an ICE.
+    """
+    return _ciaaw.get_ice(str(s), int(A), bool(uncertainty))
