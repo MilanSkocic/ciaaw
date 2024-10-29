0aee1119dcd343c1b89c82f6d6114e9d22313d11
Milan Skocic <milan.skocic@icloud.com>
2024-10-29 07:06:03 +0100

    Update Python example.

--- example/example.py
+++ example/example.py
@@ -7,10 +7,10 @@ print("########## CIAAW VERSION ##########")
 print("version ", pyciaaw.__version__)
 
 print("########## CIAAW SAW  ##########")
-print("Abridged Standard Atomic Weight: ", pyciaaw.get_saw("H"))
-print("Uncertainty for abridged Standard Atomic Weight: ", pyciaaw.get_saw("H", uncertainty=True))
-print("Standard Atomic Weight: ", pyciaaw.get_saw("H", abridged=False, uncertainty=False))
-print("Uncertainty for Standard Atomic Weight: ", pyciaaw.get_saw("H", abridged=False, uncertainty=True))
+print("ASAW     = ", pyciaaw.get_saw("H"))
+print("U ASAW   = ", pyciaaw.get_saw("H", uncertainty=True))
+print("SAW      = ", pyciaaw.get_saw("H", abridged=False, uncertainty=False))
+print("U SAW    = ", pyciaaw.get_saw("H", abridged=False, uncertainty=True))
 
 
 print("########## CIAAW ICE  ##########")
@@ -19,3 +19,6 @@ print('ICE H 1   = ', pyciaaw.get_ice("H", A=1))
 print('U ICE H 1 = ', pyciaaw.get_ice("H", A=1, uncertainty=True))
 print('ICE H 2   = ', pyciaaw.get_ice("H", A=2))
 print('U ICE H 2 = ', pyciaaw.get_ice("H", A=2, uncertainty=True))
+
+print("ASAW     = ", pyciaaw.get_saw("Tc"))
+print("N ICE Tc   = ", pyciaaw.get_nice("Tc"))
