948df64471ad5096258b641f0573ddf3c923f1f6
Milan Skocic <milan.skocic@icloud.com>
2024-11-05 07:25:19 +0100

    Update script for NAW.
    Cleaned file with header.

--- scripts/naw/format_iupac.py
+++ scripts/naw/format_iupac.py
@@ -3,7 +3,27 @@ import argparse
 import csv
 
 def run(iupac: str, iupac_clean:str)->None:
-    pass
+    
+    fclean = open(iupac_clean, "w")
+
+    with open(iupac, "r") as csvfile:
+        rows = csv.reader(csvfile)
+        
+        k = 1
+        for row in rows:
+            if k == 3:
+                nuclide, value, uncertainty, year = row
+                headers = ",".join(row)
+                fclean.write(f"{nuclide:10s},{value:20s},{uncertainty:20s},{year:10s}\n")
+            if k > 4:
+                row[-1] = row[-1].split(">")[1].split("<")[0]
+            if row[-1] == "2020":
+                nuclide, value, uncertainty, year = row
+                fclean.write(f"{nuclide:10s},{value:20s},{uncertainty:20s},{year:10s}\n")
+            
+            k += 1
+
+    fclean.close()
     
 
 if __name__ == "__main__":
