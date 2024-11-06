a0a87fef1805786ebe8ead6f15689f7b3e971409
Milan Skocic <milan.skocic@icloud.com>
2024-11-06 07:18:28 +0100

    Update NAW script for sorting elements.

--- scripts/naw/format_iupac.py
+++ scripts/naw/format_iupac.py
@@ -6,22 +6,34 @@ def run(iupac: str, iupac_clean:str)->None:
     
     fclean = open(iupac_clean, "w")
 
+    elements = ["H", "He"] 
+
     with open(iupac, "r") as csvfile:
         rows = csv.reader(csvfile)
         
-        k = 1
-        for row in rows:
-            if k == 3:
-                nuclide, value, uncertainty, year = row
-                headers = ",".join(row)
-                fclean.write(f"{nuclide:10s},{value:20s},{uncertainty:20s},{year:10s}\n")
-            if k > 4:
-                row[-1] = row[-1].split(">")[1].split("<")[0]
-            if row[-1] == "2020":
-                nuclide, value, uncertainty, year = row
-                fclean.write(f"{nuclide:10s},{value:20s},{uncertainty:20s},{year:10s}\n")
-            
-            k += 1
+        for e in elements:
+            k = 1
+            print(e)
+            for row in rows:
+                if k == 3:
+                    nuclide, value, uncertainty, year = row
+                    headers = ",".join(row)
+                    fclean.write(f"{nuclide:10s},{value:20s},{uncertainty:20s},{year:10s}\n")
+                
+                if k > 4 :
+                    nuclide, value, uncertainty, year = row
+                    row[-1] = row[-1].split(">")[1].split("<")[0]
+                    
+                    if row[-1] == "2020":
+                        for i in range(1, len(nuclide)+1):
+                            if not nuclide[:i].isdigit():
+                                break
+                        
+                        if nuclide[i-1:] == e:
+                            print(nuclide[i-1:])
+                            fclean.write(f"{nuclide:10s},{value:20s},{uncertainty:20s},{year:10s}\n")
+                
+                k += 1
 
     fclean.close()
     
