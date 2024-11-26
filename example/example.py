c13f9f3f8ae59ca158666cdaccc53bd533f3cbb6
Milan Skocic <milan.skocic@icloud.com>
2024-11-26 07:32:32 +0100

    Update python example with NAW.

--- example/example.py
+++ example/example.py
@@ -11,6 +11,7 @@ print("ASAW     = ", pyciaaw.get_saw("H"))
 print("U ASAW   = ", pyciaaw.get_saw("H", uncertainty=True))
 print("SAW      = ", pyciaaw.get_saw("H", abridged=False, uncertainty=False))
 print("U SAW    = ", pyciaaw.get_saw("H", abridged=False, uncertainty=True))
+print("ASAW     = ", pyciaaw.get_saw("Tc"))
 
 
 print("########## CIAAW ICE  ##########")
@@ -19,6 +20,8 @@ print('ICE H 1   = ', pyciaaw.get_ice("H", A=1))
 print('U ICE H 1 = ', pyciaaw.get_ice("H", A=1, uncertainty=True))
 print('ICE H 2   = ', pyciaaw.get_ice("H", A=2))
 print('U ICE H 2 = ', pyciaaw.get_ice("H", A=2, uncertainty=True))
-
-print("ASAW     = ", pyciaaw.get_saw("Tc"))
 print("N ICE Tc   = ", pyciaaw.get_nice("Tc"))
+
+print("########## CIAAW NAW  ##########")
+print('NAW H 2   = ', pyciaaw.get_naw("H", A=2))
+print('U NAW H 2 = ', pyciaaw.get_naw("H", A=2, uncertainty=True))
