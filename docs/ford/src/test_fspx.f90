8cb4f3687c8935384a2a449d5e0df1d6fbdeae6e
Milan Skocic <milan.skocic@icloud.com>
2025-01-30 17:26:12 +0100

    Update online documentation.

---
+++ docs/ford/src/test_fspx.f90
@@ -0,0 +1,43 @@
+module test_fspx
+    !! Module documentation
+    use iso_fortran_env
+    implicit none
+
+    real(real64), parameter :: P1 = 1.0_real64 !! Parameter docstring
+
+
+contains
+
+pure function test_1(a)result(res)
+    !! Function docstring.
+    !!
+    !! The specified result for the function appears as an argument.
+    !! Multi-line docstring does not seem to be recognized.
+
+    
+    ! Arguments
+    real(real64), intent(in) :: a !! Argument for the function
+
+    ! Returns
+    real(real64) :: res !! Returned value
+
+    res = a + 1
+end function
+
+
+subroutine test_2(a, b)
+    !! docstring
+
+    ! Arguments
+    real(real64), intent(in) :: a !! Argument a
+    real(real64), intent(out) :: b !! Argument b
+
+    ! local variables
+    real(real64) :: local_i  !! Appears as argument even if it is a local variable
+    character(len=25) :: s !! Appears as argument even if it is a local variable
+
+end subroutine
+
+
+end module
+
