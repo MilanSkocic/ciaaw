662ac1ef228fd9443f7dc8fb61ba78b257af0261
Milan Skocic <milan.skocic@icloud.com>
2025-03-09 09:35:48 +0100

    Update version.

--- src/ciaaw_version.f90
+++ src/ciaaw_version.f90
@@ -2,5 +2,5 @@ module ciaaw__version
     !! Version
     implicit none
     private
-    character(len=*), parameter, public :: version = "0.5.1dev0"
+    character(len=*), parameter, public :: version = "0.5.2dev0"
 end module ciaaw__version
