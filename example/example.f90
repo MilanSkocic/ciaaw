a358db547dcc9a428e65f3cea09347bcf3b040c9
Milan Skocic <milan.skocic@gmail.com>
2026-01-17 20:33:11 +0100

    Fix alignment in fortran example.

--- example/example.f90
+++ example/example.f90
@@ -22,17 +22,17 @@ program example_in_f
     print '(A10, F10.5)', 'ASAW T   = ', get_saw("Tc", abridged=.true.)
     
     print '(A)', '########### CIAAW ICE ##########'
-    print '(A, I3)',        'N ICE H    = ', get_nice("H")
+    print '(A, I3)',      'N ICE H    = ', get_nice("H")
     print '(A, F12.6)',   'ICE H 1    = ', get_ice("H", A=1)
     print '(A, ES23.16)', 'U ICE H 1  = ', get_ice("H", A=1, uncertainty=.true.)
     print '(A, F12.6)',   'ICE H 2    = ', get_ice("H", A=2)
     print '(A, ES23.16)', 'U ICE H 2  = ', get_ice("H", A=2, uncertainty=.true.)
-    print '(A, I3)',        'N ICE Tc   = ', get_nice("Tc")
-    print '(A, I3)',        'N ICE C    = ', get_nice("C")
+    print '(A, I3)',      'N ICE Tc   = ', get_nice("Tc")
+    print '(A, I3)',      'N ICE C    = ', get_nice("C")
     
     print '(A)', '########### CIAAW NAW ##########'
-    print '(A, ES23.16)',   'NAW H 2     = ', get_naw("H", A=2)
-    print '(A, ES23.16)',   'U NAW H 2   = ', get_naw("H", A=2, uncertainty=.true.)
-    print '(A, I3)',          'N NAW Tc    = ', get_nnaw("Tc")
+    print '(A, ES23.16)', 'NAW H 2     = ', get_naw("H", A=2)
+    print '(A, ES23.16)', 'U NAW H 2   = ', get_naw("H", A=2, uncertainty=.true.)
+    print '(A, I3)',      'N NAW Tc    = ', get_nnaw("Tc")
 
 end program
