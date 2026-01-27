698a9b18149e50d149bf2bf18a08a246421477de
Milan Skocic <milan.skocic@gmail.com>
2026-01-27 06:44:27 +0100

    Prepare new version.

--- src/ciaaw_version.f90
+++ src/ciaaw_version.f90
@@ -2,5 +2,5 @@ module ciaaw__version
     !! Version
     implicit none
     private
-    character(len=*), parameter, public :: version = "1.1.1"
+    character(len=*), parameter, public :: version = "1.1.2"
 end module ciaaw__version
