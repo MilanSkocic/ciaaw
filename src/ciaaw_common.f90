562b1a1a2fc2bf3503e4b80160f423faaa42c072
Milan Skocic <milan.skocic@gmail.com>
2026-03-04 06:29:42 +0100

    Refactoring common module.

--- src/ciaaw_common.f90
+++ src/ciaaw_common.f90
@@ -1,11 +1,10 @@
 module ciaaw__common
-    !! Common parameters
-    use stdlib_kinds, only: int64, dp, int32, sp
-    use stdlib_optval, only: optval
-    use ieee_arithmetic, only: ieee_quiet_nan, ieee_value
-    private
-
-    public :: optval
-    public :: sp, dp, int32, int64
-
-end module
+!! Common parameters
+use stdlib_kinds, only: int64, dp, int32, sp
+use stdlib_optval, only: optval
+use ieee_arithmetic, only: ieee_quiet_nan, ieee_value
+implicit none(type,external)
+private
+public :: optval
+public :: sp, dp, int32, int64
+end module ciaaw__common
