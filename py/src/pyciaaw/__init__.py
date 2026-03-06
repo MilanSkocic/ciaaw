467879a4ff610d53c5973e9be2e3a6b9bc6d15b4
Milan Skocic <milan.skocic@gmail.com>
2026-03-06 01:46:33 +0100

    Refactoring __init__.py in pywrapper.

--- py/src/pyciaaw/__init__.py
+++ py/src/pyciaaw/__init__.py
@@ -13,6 +13,7 @@ from pathlib import Path
 from ._ciaaw import __version__
 from . import _ciaaw
 
+
 def main():
     binary = Path(__file__).parent / "ciaaw"
     if platform.system() == "Windows":
@@ -21,7 +22,8 @@ def main():
     res = subprocess.run([binary] + sys.argv[1:])
     sys.exit(res.returncode)
 
-def get_saw(s: str, abridged: bool=True, uncertainty: bool=False)->float:
+
+def get_saw(s: str, abridged: bool = True, uncertainty: bool = False) -> float:
     r"""
     Get the standard atomic weight of the element s.
 
@@ -37,13 +39,15 @@ def get_saw(s: str, abridged: bool=True, uncertainty: bool=False)->float:
     Returns:
     --------
     saw: float
-        Returns NaN if the provided element is incorrect or -1 if the element does not have a standard atomic weight.
+        Returns NaN if the provided element is incorrect or -1
+        if the element does not have a standard atomic weight.
     """
     return _ciaaw.get_saw(str(s), bool(abridged), bool(uncertainty))
 
-def get_ice(s:str, A:int, uncertainty: bool=False)->float:
+
+def get_ice(s: str, A: int, uncertainty: bool = False) -> float:
     r"""
-    Get the isotopic composition of the element s for the mass number A. 
+    Get the isotopic composition of the element s for the mass number A.
 
     Parameters
     ----------
@@ -52,16 +56,19 @@ def get_ice(s:str, A:int, uncertainty: bool=False)->float:
     A: int
         Mass number.
     uncertainty: bool, optional
-        Flag to get the uncertainty instead of the value. Default to False.
+        Flag to get the uncertainty instead of the value.
+        Default to False.
 
     Returns
     -------
     ice: float
-        Returns NaN if the provided element or the mass number A are incorrect or -1 if the element does not have an ICE.
+        Returns NaN if the provided element or the mass number A are
+        incorrect or -1 if the element does not have an ICE.
     """
     return _ciaaw.get_ice(str(s), int(A), bool(uncertainty))
 
-def get_nice(s:str)->int:
+
+def get_nice(s: str) -> int:
     r"""
     Get the number of isotopes in ICE of the element s.
 
@@ -69,7 +76,7 @@ def get_nice(s:str)->int:
     ----------
     s: str
         Element symbol.
-    
+
     Returns
     -------
     nice: int
@@ -78,9 +85,9 @@ def get_nice(s:str)->int:
     return _ciaaw.get_nice(str(s))
 
 
-def get_naw(s:str, A:int, uncertainty: bool=False)->float:
+def get_naw(s: str, A: int, uncertainty: bool = False) -> float:
     r"""
-    Get the atomic weight of the nuclide s for the mass number A. 
+    Get the atomic weight of the nuclide s for the mass number A.
 
     Parameters
     ----------
@@ -94,11 +101,13 @@ def get_naw(s:str, A:int, uncertainty: bool=False)->float:
     Returns
     -------
     naw: float
-        Returns NaN if the provided element or A are incorrect or -1 if the element does not have an NAW.
+        Returns NaN if the provided element or A are incorrect or -1
+        if the element does not have an NAW.
     """
     return _ciaaw.get_naw(str(s), int(A), bool(uncertainty))
 
-def get_nnaw(s:str)->int:
+
+def get_nnaw(s: str) -> int:
     r"""
     Get the number of nuclides in NAW of the element s.
 
@@ -106,7 +115,7 @@ def get_nnaw(s:str)->int:
     ----------
     s: str
         Element symbol.
-    
+
     Returns
     -------
     nnaw: int
