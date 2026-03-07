f016aa282904d120cd3de984d739e0d7af2a0bce
Milan Skocic <milan.skocic@gmail.com>
2026-03-07 08:14:15 +0100

    Update examples in prep documentation.

--- src/ciaaw.f90
+++ src/ciaaw.f90
@@ -160,29 +160,29 @@
 !     Example in Fortran
 ! 
 !         program example_in_f
-!         use ciaaw, only: get_saw, get_ice, get_naw, get_nice, get_nnaw
+!         use ciaaw, only: get_saw, get_ice, get_naw, get_nice, get_nnaw, get_version
 !         implicit none(type,external)
 !         print *, "version ", get_version()
 ! 
 !         print '(A)', '########### CIAAW SAW ##########'
-!         print '(A10, F10.5)', 'ASAW H = ', get_saw("H", abridged=.true.)
-!         print '(A10, F10.5)', 'U ASAW H = ', get_saw("H", uncertainty=.true.)
-!         print '(A10, F10.5)', 'SAW H = ', get_saw("H", abridged = .false.)
-!         print '(A10, F10.5)', 'U SAW H = ', get_saw("H", abridged = .false., uncertainty = .true.)
-!         print '(A10, F10.5)', 'ASAW T = ', get_saw("Tc", abridged=.true.)
+!         print '(A10, F10.5)', 'ASAW H = ', get_saw("H", ab=.true.)
+!         print '(A10, F10.5)', 'U ASAW H = ', get_saw("H", u=.true.)
+!         print '(A10, F10.5)', 'SAW H = ', get_saw("H", ab=.false.)
+!         print '(A10, F10.5)', 'U SAW H = ', get_saw("H", ab=.false., u=.true.)
+!         print '(A10, F10.5)', 'ASAW T = ', get_saw("Tc", ab=.true.)
 ! 
 !         print '(A)', '########### CIAAW ICE ##########'
 !         print '(A, I3)', 'N ICE H = ', get_nice("H")
 !         print '(A, F12.6)', 'ICE H 1 = ', get_ice("H", A=1)
-!         print '(A, ES23.16)', 'U ICE H 1 = ', get_ice("H", A=1, uncertainty=.true.)
+!         print '(A, ES23.16)', 'U ICE H 1 = ', get_ice("H", A=1, u=.true.)
 !         print '(A, F12.6)', 'ICE H 2 = ', get_ice("H", A=2)
-!         print '(A, ES23.16)', 'U ICE H 2 = ', get_ice("H", A=2, uncertainty=.true.)
+!         print '(A, ES23.16)', 'U ICE H 2 = ', get_ice("H", A=2, u=.true.)
 !         print '(A, I3)', 'N ICE Tc = ', get_nice("Tc")
 !         print '(A, I3)', 'N ICE C = ', get_nice("C")
 ! 
 !         print '(A)', '########### CIAAW NAW ##########'
 !         print '(A, ES23.16)', 'NAW H 2 = ', get_naw("H", A=2)
-!         print '(A, ES23.16)', 'U NAW H 2 = ', get_naw("H", A=2, uncertainty=.true.)
+!         print '(A, ES23.16)', 'U NAW H 2 = ', get_naw("H", A=2, u=.true.)
 !         print '(A, I3)', 'N NAW Tc = ', get_nnaw("Tc")
 ! 
 !         end program example_in_f
@@ -231,24 +231,25 @@
 ! 
 !         print("########## CIAAW SAW  ##########")
 !         print("ASAW H   = ", pyciaaw.get_saw("H"))
-!         print("U ASAW H = ", pyciaaw.get_saw("H", uncertainty=True))
-!         print("SAW H    = ", pyciaaw.get_saw("H", abridged=False, uncertainty=False))
-!         print("U SAW H  = ", pyciaaw.get_saw("H", abridged=False, uncertainty=True))
+!         print("U ASAW H = ", pyciaaw.get_saw("H", u=True))
+!         print("SAW H    = ", pyciaaw.get_saw("H", ab=False, u=False))
+!         print("U SAW H  = ", pyciaaw.get_saw("H", ab=False, u=True))
 !         print("ASAW Tc  = ", pyciaaw.get_saw("Tc"))
 ! 
 !         print("########## CIAAW ICE  ##########")
 !         print("N ICE H   = ", pyciaaw.get_nice("H"))
 !         print('ICE H 1   = ', pyciaaw.get_ice("H", A=1))
-!         print('U ICE H 1 = ', pyciaaw.get_ice("H", A=1, uncertainty=True))
+!         print('U ICE H 1 = ', pyciaaw.get_ice("H", A=1, u=True))
 !         print('ICE H 2   = ', pyciaaw.get_ice("H", A=2))
-!         print('U ICE H 2 = ', pyciaaw.get_ice("H", A=2, uncertainty=True))
+!         print('U ICE H 2 = ', pyciaaw.get_ice("H", A=2, u=True))
 !         print("N ICE Tc  = ", pyciaaw.get_nice("Tc"))
 !         print("N ICE C   = ", pyciaaw.get_nice("C"))
 ! 
 !         print("########## CIAAW NAW  ##########")
 !         print('NAW H 2   = ', pyciaaw.get_naw("H", A=2))
-!         print('U NAW H 2 = ', pyciaaw.get_naw("H", A=2, uncertainty=True))
+!         print('U NAW H 2 = ', pyciaaw.get_naw("H", A=2, u=True))
 !         print("N NAW Tc  = ", pyciaaw.get_nnaw("Tc"))
+! 
 ! 
 ! SEE ALSO
 !     ciaaw(1), gsl(3), codata(3)
