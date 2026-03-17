d5adc0174403a3e14c02ff35c198d6451a5cc652
Milan Skocic <milan.skocic@gmail.com>
2026-03-17 05:36:01 +0100

    Update scripts for generating APIs directly into srcprep folder for inclusion with prep(1).

--- scripts/py2txt.sh
+++ scripts/py2txt.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
+fpath="./srcprep/pyapi.txt"
 i=0
+
+echo "    Python API:" > $fpath
 
 while IFS='' read -r lineraw; do
     line=$(echo $lineraw | sed -E 's/^[[:space:]]*//')
     case $line in
         "def"*":"*)
-            echo "        o $(echo $line | sed -E 's/def //g;;s/:*$//g')"
+            echo "        o $(echo $line | sed -E 's/def //g;;s/:*$//g')" >> $fpath
             ;;
         *)
             ;;
