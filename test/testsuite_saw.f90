0e4dcd17089db6e59e21b5879696492ed263c536
Milan Skocic <milan.skocic@gmail.com>
2026-03-20 01:19:45 +0100

    Update tests.

--- test/testsuite_saw.f90
+++ test/testsuite_saw.f90
@@ -1,11 +1,11 @@
 module testsuite_saw
     !! Test saw
-    use iso_fortran_env
     use testdrive, only : new_unittest, unittest_type, error_type, check
-    use ciaaw
-    implicit none
+    use ciaaw__common, only: int32, dp
+    use ciaaw, only: saw
+    implicit none(type,external)
     private
-    
+
     public :: collect_suite_saw
 
 contains
@@ -13,24 +13,26 @@ contains
 subroutine collect_suite_saw(testsuite)
   type(unittest_type), allocatable, intent(out) :: testsuite(:)
   testsuite = [new_unittest("ASAW", test_asaw)]
-end subroutine
+end subroutine collect_suite_saw
 
 
 subroutine test_asaw(error)
     integer(int32), parameter :: N = 3
-    type(error_type), allocatable, intent(out) :: error 
-    character(len=2) :: elmt(3) = [character(len=2) :: "H", "C", "Ne"]
-    real(real64) :: expected_asaw(N) = [1.0080d0, 12.011d0, 20.180d0]
-
+    type(error_type), allocatable, intent(out) :: error
+    character(len=2) :: elmt(3)
+    real(dp) :: expected_asaw(N)
     integer(int32) :: i
-    real(real64) :: value, expected
+    real(dp) :: value, expected
+
+    elmt = [character(len=2) :: "H", "C", "Ne"]
+    expected_asaw = [1.0080d0, 12.011d0, 20.180d0]
 
     do i=1, N
-        value = get_saw(elmt(i))
+        value = saw(elmt(i))
         expected = expected_asaw(i)
         call check(error, value, expected)
         if (allocated(error)) return
     end do
-end subroutine
+end subroutine test_asaw
 
-end module
+end module testsuite_saw
